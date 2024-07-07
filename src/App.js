8085b042bfb5922e5d3d0a7bca98869f49768af0
Aditya Singh <Aditya@Ts-MacBook-Air-2.local>
2024-07-07 13:30:22 +0530

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import OpenAI from "openai";
+console.log(process.env.REACT_APP_MY_API_KEY)
+// import {Configuration, openAIApi} from "openai";
+
+
+const openai = new OpenAI({ apiKey: "<my-api-key>", dangerouslyAllowBrowser: true });
 
 function App() {
+
+  const [image, setImage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [prompt, setPrompt] = useState("");
+
+
+  // const configuration = new Configuration({
+  //   apiKey: process.env.REACT_APP_MY_API_KEY,
+  // });
+  // const openai = new openAIApi(configuration);
+
+  async function fetchData() {
+    try{
+      const response = await openai.images.generate({
+        model: "dall-e-3",
+        prompt: "a white siamese cat",
+        n: 1,
+        size: "1024x1024",
+      });
+      var image_url = response.data[0].url;
+      console.log(image_url)
+    } catch(e){
+      console.log(e);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image Generator</h1>
+      <button onClick={fetchData}>Generate</button>
     </div>
   );
 }
